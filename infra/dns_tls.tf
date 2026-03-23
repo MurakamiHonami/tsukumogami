e035b44f7e479af0f08b2d07c320a83cb24dc5bc
tama <mnv2525@gmail.com>
2026-03-23 12:12:23 +0900

    fix deploy error

--- infra/dns_tls.tf
+++ infra/dns_tls.tf
@@ -27,6 +27,7 @@ resource "aws_route53_record" "backend_certificate_validation" {
   type    = each.value.type
   ttl     = 60
   records = [each.value.record]
+  allow_overwrite = true
 }
 
 resource "aws_acm_certificate_validation" "backend" {
@@ -59,6 +60,7 @@ resource "aws_route53_record" "frontend_certificate_validation" {
   type    = each.value.type
   ttl     = 60
   records = [each.value.record]
+  allow_overwrite = true
 }
 
 resource "aws_acm_certificate_validation" "frontend" {
